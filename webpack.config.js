2766350207a8415d196970132a17c3afc35bc5e3
DudkoSerhei <serhei.dudko@gmail.com>
2017-11-03 22:18:23 +0300

    Fix style, url, file loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/app.js',
+  entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
@@ -12,7 +12,31 @@ module.exports = {
        test: /.jsx?$/,
        loader: 'babel-loader',
        exclude: /node_modules/
-     }
+     },
+     {
+       test: /\.css$/,
+       loader: 'css-loader'
+     },
+     {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {}
+          }
+        ]
+      }
    ]
  }
 };
